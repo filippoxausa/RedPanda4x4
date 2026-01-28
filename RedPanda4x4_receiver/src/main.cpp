29df7cf61c3e0415f162da22cb3872393b057ba9
filippoxausa <filippoxausa@gmail.com>
2026-01-28 18:30:49 +0100

    fix/Controller call collision

--- RedPanda4x4_receiver/src/main.cpp
+++ RedPanda4x4_receiver/src/main.cpp
@@ -28,18 +28,20 @@ void loop() {
   nowRx.getLatest(a, ageMs);
 
   // Fail-safe: se non arrivano dati recenti, ferma
-  if (ageMs > 300) {
-    motorsStop();
-  } else {
-    MotorCmd cmd = accelToMotors(a);
+  // if (ageMs > 300) {
+  //   motorsStop();
+  // } else {
+  //   MotorCmd cmd = accelToMotors(a);
+  //   motorsSet(cmd.left, cmd.right);
+  // }
+  MotorCmd cmd = accelToMotors(a);
     motorsSet(cmd.left, cmd.right);
-  }
 
-  static uint32_t t0 = 0;
-  if (millis() - t0 > 200) {
-    t0 = millis();
-    // Serial.printf("Axyz=%d %d %d age=%lu ms\n", a.ax, a.ay, a.az, (unsigned long)ageMs);
-  }
+  // static uint32_t t0 = 0;
+  // if (millis() - t0 > 200) {
+  //   t0 = millis();
+  //   // Serial.printf("Axyz=%d %d %d age=%lu ms\n", a.ax, a.ay, a.az, (unsigned long)ageMs);
+  // }
 
   delay(5);
 }
